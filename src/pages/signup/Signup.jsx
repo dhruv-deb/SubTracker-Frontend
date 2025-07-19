7571611e1b025c9a5714d7c8904489440dac13f4
dhruv_v <jagatjyoti0810@gmail.com>
2025-07-19 18:52:07 +0530

    minor fix

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext"; 
-import styles from "./SignUp.module.scss";
+import styles from "./Signup.module.scss";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
